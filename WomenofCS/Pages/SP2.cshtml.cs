efc4cf75e6516da44b6e69d11cbf53003f576c49
Michelle Fowler <mef571.biz@gmail.com>
2021-08-15 18:20:13 -0400

    Add files via upload

---
+++ WomenofCS/Pages/SP2.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WomenofCS
+{
+    public class SP2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
